9b46750e9e50b1444acb20b85d753384c39e7774
XIN ZHAO <xinzhao@XINdeMacBook-Pro.local>
2020-07-05 23:47:05 -0500

    test

--- test/libnumaperfempty.cpp
+++ test/libnumaperfempty.cpp
@@ -1,7 +1,35 @@
-
+#include <stdlib.h>
+#include <stdio.h>
 #include "libnumaperfempty.h"
+#include "../source/utils/concurrency/automics.h"
+#include "../source/utils/log/Logger.h"
+
+unsigned long store_num;
+unsigned long load_num;
+
+thread_local unsigned long threadStoreNum = 0;
+thread_local unsigned long threadLoadNum = 0;
+
+static void initializer(void) {
+    fprintf(stderr, "Test NumaPerf initializer\n");
+    store_num = 0;
+    load_num = 0;
+}
 
 void handleAccess(unsigned long addr, size_t size, eAccessType type) {
+    if (type == E_ACCESS_READ) {
+        threadLoadNum++;
+        if (threadLoadNum > 100) {
+            Automics::automicIncrease(&load_num, threadLoadNum);
+        }
+        Logger::debug("NumaPerf handleAccess read\n");
+        return;
+    }
+    threadStoreNum++;
+    if (threadStoreNum > 100) {
+        Automics::automicIncrease(&store_num, threadStoreNum);
+    }
+    Logger::debug("NumaPerf handleAccess write\n");
 }
 
 /*
