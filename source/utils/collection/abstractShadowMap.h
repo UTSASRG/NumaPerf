5a32b08e02d98eff9882a5155c06e4b4971fb542
XIN ZHAO <xinzhao@XINdeMacBook-Pro.local>
2020-08-04 17:21:21 -0500

    reformat

---
+++ source/utils/collection/abstractShadowMap.h
@@ -0,0 +1,87 @@
+#ifndef NUMAPERF_ABSTRACTSHADOWMAP_H
+#define NUMAPERF_ABSTRACTSHADOWMAP_H
+
+#include "../mm.hh"
+#include <assert.h>
+#include "../addresses.h"
+#include "../log/Logger.h"
+#include "../concurrency/automics.h"
+
+template<class ValueType>
+class AbstractShadowMap {
+
+    void *startAddress;
+    unsigned long size;
+    unsigned long blockSize;
+    const static int META_DATA_SIZE = sizeof(short);
+    const static short NOT_INSERT = 0;
+    const static short INSERTING = 1;
+    const static short INSERTED = 2;
+
+protected:
+    virtual inline unsigned long hashKey(unsigned long key) = 0;
+
+    inline void *getDataBlock(unsigned long index) {
+        unsigned long offset = index * blockSize;
+        assert(offset < size);
+        return ((char *) startAddress) + offset;
+    }
+
+public:
+    void initialize(unsigned long size, bool needAlignToCacheLine = false) {
+        startAddress = MM::mmapAllocatePrivate(size);
+        this->size = size;
+        if (needAlignToCacheLine) {
+            Logger::debug("AlignToCacheLine, original Size:%lu, result Size:%lu \n", sizeof(ValueType) + META_DATA_SIZE,
+                          ADDRESSES::alignUpToCacheLine(sizeof(ValueType) + META_DATA_SIZE));
+            blockSize = ADDRESSES::alignUpToCacheLine(sizeof(ValueType) + META_DATA_SIZE);
+        } else {
+            Logger::debug("AlignToWord, original Size:%lu, result Size:%lu \n", sizeof(ValueType) + META_DATA_SIZE,
+                          ADDRESSES::alignUpToWord(sizeof(ValueType) + META_DATA_SIZE));
+            blockSize = ADDRESSES::alignUpToWord(sizeof(ValueType) + META_DATA_SIZE);
+        }
+    }
+
+    inline bool insertIfAbsent(const unsigned long &key, const ValueType &value) {
+        unsigned long index = hashKey(key);
+        void *dataBlock = this->getDataBlock(index);
+        short *metaData = (short *) dataBlock;
+        if (!Automics::compare_set(metaData, NOT_INSERT, INSERTING)) {
+            // busy waiting, since this could be very quick
+            while (*metaData != INSERTED) {
+                Logger::debug("shadow map insertIfAbsent busy waiting\n");
+            }
+            return false;
+        }
+        ValueType *valuePtr = (ValueType *) (((char *) dataBlock) + META_DATA_SIZE);
+        *valuePtr = value;
+        *metaData = INSERTED;
+        return true;
+    }
+
+    inline void insert(const unsigned long &key, const ValueType &value) {
+        unsigned long index = hashKey(key);
+        void *dataBlock = this->getDataBlock(index);
+        ValueType *valuePtr = (ValueType *) (((char *) dataBlock) + META_DATA_SIZE);
+        *valuePtr = value;
+        short *metaData = (short *) dataBlock;
+        *metaData = INSERTED;
+    }
+
+    inline ValueType *find(const unsigned long &key) {
+        unsigned long index = hashKey(key);
+        void *dataBlock = this->getDataBlock(index);
+        if (*((short *) dataBlock) != INSERTED) {
+            return NULL;
+        }
+        return (ValueType *) (((char *) dataBlock) + META_DATA_SIZE);
+    }
+
+    inline void remove(const unsigned long &key) {
+        unsigned long index = hashKey(key);
+        void *dataBlock = this->getDataBlock(index);
+        *((short *) dataBlock) = NOT_INSERT;
+    }
+};
+
+#endif //NUMAPERF_ABSTRACTSHADOWMAP_H
--- source/utils/collection/addrtocacheindexshadowmap.h
+++ source/utils/collection/addrtocacheindexshadowmap.h
@@ -6,6 +6,7 @@
 #include "../addresses.h"
 #include "../log/Logger.h"
 #include "../concurrency/automics.h"
+#include "abstractShadowMap.h"
 
 /**
  * memory layout: bool-value-bool-value-bool-value
@@ -13,81 +14,11 @@
  * @tparam ValueType
  */
 template<class ValueType>
-class AddressToCacheIndexShadowMap {
-
-    void *startAddress;
-    unsigned long size;
-    unsigned long blockSize;
-    const static int META_DATA_SIZE = sizeof(short);
-    const static short NOT_INSERT = 0;
-    const static short INSERTING = 1;
-    const static short INSERTED = 2;
+class AddressToCacheIndexShadowMap : public AbstractShadowMap<ValueType> {
 
 private:
-    inline unsigned long hashKey(unsigned long key) {
+    inline unsigned long hashKey(unsigned long key) override {
         return ADDRESSES::getCacheIndex(key);
-    }
-
-    inline void *getDataBlock(unsigned long index) {
-        unsigned long offset = index * blockSize;
-        assert(offset < size);
-        return ((char *) startAddress) + offset;
-    }
-
-public:
-    void initialize(unsigned long size, bool needAlignToCacheLine = false) {
-        startAddress = MM::mmapAllocatePrivate(size);
-        this->size = size;
-        if (needAlignToCacheLine) {
-            Logger::debug("AlignToCacheLine, original Size:%lu, result Size:%lu \n", sizeof(ValueType) + META_DATA_SIZE,
-                          ADDRESSES::alignUpToCacheLine(sizeof(ValueType) + META_DATA_SIZE));
-            blockSize = ADDRESSES::alignUpToCacheLine(sizeof(ValueType) + META_DATA_SIZE);
-        } else {
-            Logger::debug("AlignToWord, original Size:%lu, result Size:%lu \n", sizeof(ValueType) + META_DATA_SIZE,
-                          ADDRESSES::alignUpToWord(sizeof(ValueType) + META_DATA_SIZE));
-            blockSize = ADDRESSES::alignUpToWord(sizeof(ValueType) + META_DATA_SIZE);
-        }
-    }
-
-    inline bool insertIfAbsent(const unsigned long &key, const ValueType &value) {
-        unsigned long index = hashKey(key);
-        void *dataBlock = this->getDataBlock(index);
-        short *metaData = (short *) dataBlock;
-        if (!Automics::compare_set(metaData, NOT_INSERT, INSERTING)) {
-            // busy waiting, since this could be very quick
-            while (*metaData != INSERTED) {
-                Logger::debug("shadow map insertIfAbsent busy waiting\n");
-            }
-            return false;
-        }
-        ValueType *valuePtr = (ValueType *) (((char *) dataBlock) + META_DATA_SIZE);
-        *valuePtr = value;
-        *metaData = INSERTED;
-        return true;
-    }
-
-    inline void insert(const unsigned long &key, const ValueType &value) {
-        unsigned long index = hashKey(key);
-        void *dataBlock = this->getDataBlock(index);
-        ValueType *valuePtr = (ValueType *) (((char *) dataBlock) + META_DATA_SIZE);
-        *valuePtr = value;
-        short *metaData = (short *) dataBlock;
-        *metaData = INSERTED;
-    }
-
-    inline ValueType *find(const unsigned long &key) {
-        unsigned long index = hashKey(key);
-        void *dataBlock = this->getDataBlock(index);
-        if (*((short *) dataBlock) != INSERTED) {
-            return NULL;
-        }
-        return (ValueType *) (((char *) dataBlock) + META_DATA_SIZE);
-    }
-
-    inline void remove(const unsigned long &key) {
-        unsigned long index = hashKey(key);
-        void *dataBlock = this->getDataBlock(index);
-        *((short *) dataBlock) = NOT_INSERT;
     }
 };
 
